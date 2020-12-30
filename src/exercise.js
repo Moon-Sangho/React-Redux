2d21417f7c5922f6b57008f5f7cadc08cb7a4cef
Moon-Sangho <skym887@gmail.com>
2020-12-30 16:41:21 +0900

    초기 세팅 & Redux tutorial

---
+++ src/exercise.js
@@ -0,0 +1,79 @@
+import { createStore } from "redux";
+
+const initialState = {
+  counter: 0,
+  text: "",
+  list: [],
+};
+
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+const CHANGE_TEXT = "CHANGE_TEXT";
+const ADD_TO_LIST = "ADD_TO_LIST";
+
+const increase = () => ({
+  type: INCREASE,
+});
+
+const decrease = () => ({
+  type: DECREASE,
+});
+
+const changeText = (text) => ({
+  type: CHANGE_TEXT,
+  text,
+});
+
+const addToList = (item) => ({
+  type: ADD_TO_LIST,
+  item,
+});
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case CHANGE_TEXT:
+      return {
+        ...state,
+        text: action.text,
+      };
+    case ADD_TO_LIST:
+      return {
+        ...state,
+        list: state.list.concat(action.item),
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+console.log(store.getState());
+
+const listener = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+const unsubscribe = store.subscribe(listener);
+
+store.dispatch(increase());
+store.dispatch(decrease());
+store.dispatch(changeText("안녕하세요"));
+store.dispatch(addToList({ id: 1, text: "와우" }));
+
+// 아래와 같이 설정하면 console 창에서 dispatch 기능을 테스트해볼 수 있다.
+window.store = store;
+
+// 아래와 같이 설정 후 console 창에서 unsubscribe(); 를 입력한 후 dispatch를 해보면 상태 변화를 확인할 수 없다.
+// (변화는 있지만 눈에 보이지 않는 것)
+window.unsubscribe = unsubscribe;
